9ab37331bec31d6e2cbe9ef3dc6b0298b6ac5393
Frederikke <frfl0001@stud.kea.dk>
2023-08-25 12:55:14 +0200

    superhelt

---
+++ superhelt.js
@@ -0,0 +1,16 @@
+{
+  powers: ["Finding things", "Summoning"];
+  realName: "Olivia Hennesey";
+  alias: "The Obtainer";
+  origin: "Earth";
+  weaknesses: ["Working", "Being stressed"];
+  active: false;
+  isEvil: true;
+  height: 170;
+  birtday: {
+    day: 2;
+    month: 2;
+    year: 1996;
+  }
+  image: "https://images.unsplash.com/photo-1509710143422-ec097f958e52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2566&q=80";
+}
